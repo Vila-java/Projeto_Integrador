d794294f18e00a5eb4ab438150f8fe179360e93c
Weslley Rocha <weslley.rocha@mercadolivre.com>
2022-08-05 19:29:33 -0300

    Adicionando método findById no WarehouseService

--- src/main/java/meli/freshfood/service/WarehouseService.java
+++ src/main/java/meli/freshfood/service/WarehouseService.java
@@ -1,4 +1,9 @@
 package meli.freshfood.service;
 
+import meli.freshfood.model.Warehouse;
+
+import java.util.Optional;
+
 public interface WarehouseService {
+    Optional<Warehouse> findById(Long id);
 }
--- src/main/java/meli/freshfood/service/WarehouseServiceImpl.java
+++ src/main/java/meli/freshfood/service/WarehouseServiceImpl.java
@@ -1,7 +1,35 @@
 package meli.freshfood.service;
 
+import meli.freshfood.model.Warehouse;
+import meli.freshfood.repository.WarehouseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class WarehouseServiceImpl implements WarehouseService {
+
+    @Autowired
+    private WarehouseRepository warehouseRepository;
+
+    @Override
+    public Optional<Warehouse> findById(Long id) {
+        Optional<Warehouse> warehouse = warehouseRepository.findById(id);
+
+        if (warehouse.isEmpty()) {
+            // TODO: Alterar exceção
+            throw new RuntimeException();
+        }
+
+        return warehouse;
+    }
+
+
 }
+
+
+
+
+
+
