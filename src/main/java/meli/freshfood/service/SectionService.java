a310eaa7ff5964cf22f0c14301ce741e7e0345d4
everodrigues <evelin.rodrigues@mercadolivre.com>
2022-08-05 18:38:05 -0300

    Add validações de section (se o setor é válido)

--- src/main/java/meli/freshfood/service/SectionService.java
+++ src/main/java/meli/freshfood/service/SectionService.java
@@ -1,4 +1,6 @@
 package meli.freshfood.service;
+import meli.freshfood.model.Section;
 
 public interface SectionService {
+    boolean isSectionValid(Section section);
 }
--- src/main/java/meli/freshfood/service/SectionServiceImpl.java
+++ src/main/java/meli/freshfood/service/SectionServiceImpl.java
@@ -1,7 +1,19 @@
 package meli.freshfood.service;
-
+import meli.freshfood.model.Section;
+import meli.freshfood.repository.SectionRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 public class SectionServiceImpl implements SectionService {
+
+    @Autowired
+    SectionRepository sectionRepository;
+
+    public boolean isSectionValid(Section section) {
+        if (section != null) {
+            return true;
+        }
+        return false;
+    }
 }
