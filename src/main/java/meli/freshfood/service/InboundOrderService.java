1e1a1accaa3f33f656da57516aedf025b0f18ab4
matheusaalves <matheus.aalves@mercadolivre.com>
2022-08-05 19:19:06 -0300

    Adicionando método update no InboundOrderService

--- src/main/java/meli/freshfood/service/InboundOrderService.java
+++ src/main/java/meli/freshfood/service/InboundOrderService.java
@@ -1,7 +1,8 @@
 package meli.freshfood.service;
 
+import meli.freshfood.dto.InboundOrderDTO;
 import meli.freshfood.model.InboundOrder;
 
 public interface InboundOrderService {
-    InboundOrder update();
+    InboundOrder update(InboundOrderDTO inboundOrderDTO);
 }
--- src/main/java/meli/freshfood/service/InboundOrderServivceImpl.java
+++ src/main/java/meli/freshfood/service/InboundOrderServivceImpl.java
@@ -1,11 +1,12 @@
 package meli.freshfood.service;
 
+import meli.freshfood.dto.InboundOrderDTO;
 import meli.freshfood.model.InboundOrder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class InboundOrderServivceImpl implements InboundOrderService {
-    public InboundOrder update() {
-
+    public InboundOrder update(InboundOrderDTO inboundOrderDTO) {
+        return null;
     }
 }
