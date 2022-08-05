d55273b28fc16d2e9a8846b3466c47224c260c5e
Weslley Rocha <weslley.rocha@mercadolivre.com>
2022-08-05 19:28:51 -0300

    Adicionando método findById no SupervisorService

--- src/main/java/meli/freshfood/service/SupervisorServiceImpl.java
+++ src/main/java/meli/freshfood/service/SupervisorServiceImpl.java
@@ -1,7 +1,35 @@
 package meli.freshfood.service;
 
+import meli.freshfood.model.Supervisor;
+import meli.freshfood.model.Warehouse;
+import meli.freshfood.repository.SupervisorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class SupervisorServiceImpl implements SupervisorService {
+
+    @Autowired
+    private SupervisorRepository supervisorRepository;
+
+    @Override
+    public Optional<Supervisor> findById(Long id) {
+        Optional<Supervisor> supervisor = supervisorRepository.findById(id);
+
+        if (supervisor.isEmpty()) {
+            // TODO: Alterar exceção
+            throw new RuntimeException();
+        }
+        return supervisor;
+    }
+
+    public Boolean supervisorExistsInWarehouse(Supervisor supervisor, Warehouse warehouse) {
+        if (supervisor.getSupersivorId().equals(warehouse.getWarehouseId())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
