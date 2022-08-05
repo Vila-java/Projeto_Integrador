a1cc19e446ce978fe6c1618cbfbf2d31e74f4405
everodrigues <evelin.rodrigues@mercadolivre.com>
2022-08-05 18:37:19 -0300

    Add validações de warehouse (se o armazem existe e se supervisor pertece ao armazem)

--- src/main/java/meli/freshfood/service/WarehouseService.java
+++ src/main/java/meli/freshfood/service/WarehouseService.java
@@ -1,4 +1,6 @@
 package meli.freshfood.service;
 
 public interface WarehouseService {
+    boolean isValid(Long warehouseId);
+    boolean supervisorBelongsToWarehouse (Long supervisorId);
 }
--- src/main/java/meli/freshfood/service/WarehouseServiceImpl.java
+++ src/main/java/meli/freshfood/service/WarehouseServiceImpl.java
@@ -1,7 +1,34 @@
 package meli.freshfood.service;
 
+import meli.freshfood.model.Warehouse;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WarehouseServiceImpl implements WarehouseService {
+
+    Warehouse warehouse = new Warehouse();
+
+    public boolean isValid(Long warehouseId)
+    {
+        boolean valid = false;
+
+        if(warehouse.getWarehouseId().equals(warehouseId))
+        {
+            valid = true;
+        }
+
+        return valid;
+    }
+
+    public boolean supervisorBelongsToWarehouse (Long supervisorId)
+    {
+        boolean belongs = false;
+
+        if(warehouse.getSupervisor().getSupersivorId().equals(supervisorId))
+        {
+            belongs = true;
+        }
+
+        return belongs;
+    }
 }
