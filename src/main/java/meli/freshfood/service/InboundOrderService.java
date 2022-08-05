640dd0c2b5643beecb2680622dd2b0b5f70551c9
SamanthaPiresLuchmannLeal <samantha.luchmann@mercadolivre.com>
2022-08-05 17:35:28 -0300

    Add post inboundOrder da camada Service

--- src/main/java/meli/freshfood/service/InboundOrderService.java
+++ src/main/java/meli/freshfood/service/InboundOrderService.java
@@ -3,5 +3,5 @@ package meli.freshfood.service;
 import meli.freshfood.model.InboundOrder;
 
 public interface InboundOrderService {
-    InboundOrder update();
+    InboundOrder create(InboundOrder inboundOrder);
 }
--- src/main/java/meli/freshfood/service/InboundOrderServiceImpl.java
+++ src/main/java/meli/freshfood/service/InboundOrderServiceImpl.java
@@ -1,14 +1,19 @@
 package meli.freshfood.service;
 
 import meli.freshfood.model.InboundOrder;
+import meli.freshfood.repository.InboundOrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
-public class InboundOrderServivceImpl implements InboundOrderService {
+public class InboundOrderServiceImpl implements InboundOrderService {
 
-    public InboundOrder save(){
+    @Autowired
+    private InboundOrderRepository inboundOrderRepository;
 
+    public InboundOrder create(InboundOrder inboundOrder){
+        return inboundOrderRepository.save(inboundOrder);
     }
-    public InboundOrder update() {
-    }
+
 }
