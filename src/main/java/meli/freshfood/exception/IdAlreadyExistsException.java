0e5b5f29e7c7d58b510e842ef36718230c91cb27
Weslley Rocha <weslley.rocha@mercadolivre.com>
2022-08-08 16:10:59 -0300

    Adicionando Exceptions

---
+++ src/main/java/meli/freshfood/exception/IdAlreadyExistsException.java
@@ -0,0 +1,12 @@
+package meli.freshfood.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public class IdAlreadyExistsException extends RuntimeException {
+		public IdAlreadyExistsException(String message){
+			super(message);
+		}
+	}
+
