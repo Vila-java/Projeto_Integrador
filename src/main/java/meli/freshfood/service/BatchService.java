91c9b533fb1e5b420eb705efd908291a3d992901
Weslley Rocha <weslley.rocha@mercadolivre.com>
2022-08-08 16:01:22 -0300

    Adicionando método findById

--- src/main/java/meli/freshfood/service/BatchService.java
+++ src/main/java/meli/freshfood/service/BatchService.java
@@ -1,4 +1,9 @@
 package meli.freshfood.service;
 
+import meli.freshfood.model.Batch;
+
+import java.util.Optional;
+
 public interface BatchService {
+	Optional<Batch> findById(Long id);
 }
--- src/main/java/meli/freshfood/service/BatchServiceImpl.java
+++ src/main/java/meli/freshfood/service/BatchServiceImpl.java
@@ -1,8 +1,11 @@
 package meli.freshfood.service;
 
-import meli.freshfood.model.Product;
-import meli.freshfood.model.Section;
+import meli.freshfood.model.Batch;
+import meli.freshfood.repository.BatchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class BatchServiceImpl implements BatchService {
