b7f0d40b0621d68a0fd9a5bb53c9c7fef18a6bbc
Weslley Rocha <weslley.rocha@mercadolivre.com>
2022-08-08 16:06:39 -0300

    Adicionando Exceptions

--- src/main/java/meli/freshfood/exception/NotFoundException.java
+++ src/main/java/meli/freshfood/exception/NotFoundException.java
@@ -1,4 +1,11 @@
 package meli.freshfood.exception;
 
-public class NotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {
+	public NotFoundException(String message){
+		super(message);
+	}
 }
--- src/main/java/meli/freshfood/exception/handler/ExceptionGenericsHandler.java
+++ src/main/java/meli/freshfood/exception/handler/ExceptionGenericsHandler.java
@@ -1,4 +1,42 @@
 package meli.freshfood.exception.handler;
 
+
+import meli.freshfood.exception.BadRequestException;
+import meli.freshfood.exception.ExceptionDetails;
+import meli.freshfood.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
 public class ExceptionGenericsHandler {
+	@ExceptionHandler(NotFoundException.class)
+	public ResponseEntity<ExceptionDetails> handlerNotFoundException(Exception ex) {
+		return new ResponseEntity<>(
+				ExceptionDetails.builder()
+						.title("Not found")
+						.status(HttpStatus.NOT_FOUND.value())
+						.message(ex.getMessage())
+						.timestamp(LocalDateTime.now())
+						.build(),
+				HttpStatus.NOT_FOUND
+		);
+	}
+	@ExceptionHandler(BadRequestException.class)
+	public ResponseEntity<ExceptionDetails> handlerBadRequestException(Exception ex) {
+		return new ResponseEntity<>(
+				ExceptionDetails.builder()
+						.title("Bad Request")
+						.status(HttpStatus.BAD_REQUEST.value())
+						.message(ex.getMessage())
+						.timestamp(LocalDateTime.now())
+						.build(),
+				HttpStatus.BAD_REQUEST
+		);
+	}
+
+
 }
