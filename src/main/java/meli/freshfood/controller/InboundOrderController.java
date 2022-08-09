4abdce1b0e11328c86636d34d58e2df0b2ced7a7
matheusaalves <matheus.aalves@mercadolivre.com>
2022-08-09 10:45:09 -0300

    Alterando retornos nos métodos create e update da controller

--- src/main/java/meli/freshfood/controller/InboundOrderController.java
+++ src/main/java/meli/freshfood/controller/InboundOrderController.java
@@ -1,5 +1,6 @@
 package meli.freshfood.controller;
 
+import meli.freshfood.dto.BatchDTO;
 import meli.freshfood.dto.InboundOrderDTO;
 import meli.freshfood.model.InboundOrder;
 import meli.freshfood.service.InboundOrderService;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/fresh-products")
@@ -16,14 +19,15 @@ public class InboundOrderController {
     private InboundOrderService service;
 
     @PostMapping("/inboundorder")
-    public ResponseEntity<InboundOrder> create(@RequestBody InboundOrderDTO inboundOrderDTO) {
+    public ResponseEntity<List<BatchDTO>> create(@RequestBody InboundOrderDTO inboundOrderDTO) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(service.create(inboundOrderDTO));
     }
 
     @PutMapping("/inboundorder")
-    public ResponseEntity<InboundOrder> update(@RequestBody InboundOrderDTO inboundOrderDTO) {
-        return ResponseEntity.ok(service.update(inboundOrderDTO));
+    public ResponseEntity<List<BatchDTO>> update(@RequestBody InboundOrderDTO inboundOrderDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.update(inboundOrderDTO));
     }
 
 
