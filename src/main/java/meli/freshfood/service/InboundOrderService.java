fb48c4296844cf6327692fc38ac3349036079310
matheusaalves <matheus.aalves@mercadolivre.com>
2022-08-09 10:51:45 -0300

    Finalizando o método update e adicionando o método create no InboundOrderService

--- src/main/java/meli/freshfood/service/InboundOrderService.java
+++ src/main/java/meli/freshfood/service/InboundOrderService.java
@@ -1,9 +1,12 @@
 package meli.freshfood.service;
 
+import meli.freshfood.dto.BatchDTO;
 import meli.freshfood.dto.InboundOrderDTO;
 import meli.freshfood.model.InboundOrder;
 
+import java.util.List;
+
 public interface InboundOrderService {
-    InboundOrder create(InboundOrderDTO inboundOrderDTO);
-    InboundOrder update(InboundOrderDTO inboundOrderDTO);
+    List<BatchDTO> create(InboundOrderDTO inboundOrderDTO);
+    List<BatchDTO> update(InboundOrderDTO inboundOrderDTO);
 }
--- src/main/java/meli/freshfood/service/InboundOrderServiceImpl.java
+++ src/main/java/meli/freshfood/service/InboundOrderServiceImpl.java
@@ -1,10 +1,18 @@
 package meli.freshfood.service;
 
+import meli.freshfood.dto.BatchDTO;
 import meli.freshfood.dto.InboundOrderDTO;
 import meli.freshfood.model.*;
 import meli.freshfood.repository.InboundOrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.TimeZone;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -28,34 +36,66 @@ public class InboundOrderServiceImpl implements InboundOrderService {
     @Autowired
     private BatchService batchService;
 
-    public InboundOrder create(InboundOrderDTO inboundOrderDTO){
-
-//        return inboundOrderRepository.save(inboundOrderDTO);
-        return null;
-    }
-
-    public InboundOrder update(InboundOrderDTO inboundOrderDTO) {
+    @Transactional
+    public List<BatchDTO> create(InboundOrderDTO inboundOrderDTO){
+        Section section = sectionService.findById(inboundOrderDTO.getSection().getSectionCode()).get();
         Warehouse warehouse = warehouseService.findById(inboundOrderDTO.getSection().getWarehouseCode()).get();
         Supervisor supervisor = supervisorService.findById(inboundOrderDTO.getSupervisorId()).get();
-        Section section = sectionService.findById(inboundOrderDTO.getSection().getSectionCode()).get();
+        InboundOrder inboundOrder = new InboundOrder(inboundOrderDTO, supervisor, section);
+        inboundOrderRepository.save(inboundOrder);
 
         supervisorService.supervisorExistsInWarehouse(supervisor, warehouse);
         sectionService.checkSectionAvailableToStock(section, inboundOrderDTO.getBatchStock());
         sectionService.checkSectionBelongsToWarehouse(section,warehouse);
 
-        inboundOrderDTO.getBatchStock().stream().forEach((batchDTO) -> {
+        List<Batch> batches = inboundOrderDTO.getBatchStock().stream().map((batchDTO) -> {
             Product product = productService.findById(batchDTO.getProductId()).get();
             sectionService.checkSectionStorageTypeIsEqualProductStorageType(section, product);
-            batchService.findById(batchDTO.getBatchNumber());
-        });
+            Batch batch = new Batch(batchDTO, product, section, inboundOrder);
+            return batch;
+        }).collect(Collectors.toList());
 
-        inboundOrderDTO.getBatchStock().stream().forEach((batchDTO)-> {
+        List<BatchDTO> batchesDTO = batches.stream().map((batch)-> {
+            batchService.save(batch);
+            return batch.toDTO();
+        }).collect(Collectors.toList());
+
+        return batchesDTO;
+    }
+
+    // TODO: Testar com mais de um produto e verificar se só passa se todos os produtos estiverem corretos
+    // TODO: Validar se o lote corresponde ao inboundOrder
+    @Transactional
+    public List<BatchDTO> update(InboundOrderDTO inboundOrderDTO) {
+        InboundOrder inboundOrder = inboundOrderRepository.findById((Long)inboundOrderDTO.getOrderNumber()).get();
+        Section section = sectionService.findById(inboundOrderDTO.getSection().getSectionCode()).get();
+        Warehouse warehouse = warehouseService.findById(inboundOrderDTO.getSection().getWarehouseCode()).get();
+        Supervisor supervisor = supervisorService.findById(inboundOrderDTO.getSupervisorId()).get();
+
+        supervisorService.supervisorExistsInWarehouse(supervisor, warehouse);
+        sectionService.checkSectionAvailableToStock(section, inboundOrderDTO.getBatchStock());
+        sectionService.checkSectionBelongsToWarehouse(section,warehouse);
+
+        List<Batch> batches = inboundOrderDTO.getBatchStock().stream().map((batchDTO) -> {
+            Product product = productService.findById(batchDTO.getProductId()).get();
+            sectionService.checkSectionStorageTypeIsEqualProductStorageType(section, product);
             Batch batch = batchService.findById(batchDTO.getBatchNumber()).get();
             batch.updateByDTO(batchDTO);
+            batch.setInboundOrder(inboundOrder);
+            batch.setProduct(product);
+            return batch;
+        }).collect(Collectors.toList());
+
+        batches.stream().forEach((batch)-> {
+            batchService.save(batch);
         });
 
-        return null;
-        // TODO: arrumar o retorno.
-  //      return inboundOrderRepository.save(inboundOrder);
+        inboundOrder.setOrderDate(inboundOrderDTO.getOrderDate());
+        inboundOrderRepository.save(inboundOrder);
+
+        return inboundOrderDTO.getBatchStock();
+    }
+
+    public void validateData(InboundOrderDTO inboundOrderDTO) {
     }
 }
