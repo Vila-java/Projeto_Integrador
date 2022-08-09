f0ef582c5049728fb3d7e1088d59f08008a0fb85
BiancaPolegatti <bianca.polegatti@mercadolivre.com>
2022-08-09 15:14:51 -0300

    Transformando FK supervisor no warehouse para UK

--- src/main/java/meli/freshfood/model/Warehouse.java
+++ src/main/java/meli/freshfood/model/Warehouse.java
@@ -8,13 +8,17 @@ import java.util.List;
 
 @Data
 @Entity
+@Table(uniqueConstraints = {
+        @UniqueConstraint(name = "uniqueSupervisor", columnNames = {"supervisor_id"})
+})
 public class Warehouse {
 
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long warehouseId;
 
-   @Column(nullable = false, length = 100)
+    @Column(nullable = false, length = 100)
     private String addressCode;
 
     @OneToMany(mappedBy = "warehouse")
