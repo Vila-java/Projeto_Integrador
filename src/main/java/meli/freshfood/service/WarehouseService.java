76dc7d2af72776d2523cb8722f718476d1ff77d8
everodrigues <evelin.rodrigues@mercadolivre.com>
2022-08-10 16:39:28 -0300

    Adicionando modificações ao service de Warehouse

--- src/main/java/meli/freshfood/service/WarehouseService.java
+++ src/main/java/meli/freshfood/service/WarehouseService.java
@@ -2,8 +2,6 @@ package meli.freshfood.service;
 
 import meli.freshfood.model.Warehouse;
 
-import java.util.Optional;
-
 public interface WarehouseService {
-    Optional<Warehouse> findById(Long id);
+    Warehouse findById(Long id);
 }
