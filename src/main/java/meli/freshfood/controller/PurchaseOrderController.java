a308bf6bcb9a98368257db7a5ce945a05ffdfcaf
everodrigues <evelin.rodrigues@mercadolivre.com>
2022-08-10 16:38:08 -0300

    Adicionando rotas no controller de PurchaseOrder

--- src/main/java/meli/freshfood/controller/PurchaseOrderController.java
+++ src/main/java/meli/freshfood/controller/PurchaseOrderController.java
@@ -1,12 +1,9 @@
 package meli.freshfood.controller;
 
-import meli.freshfood.dto.ProductDTO;
+
 import meli.freshfood.dto.PurchaseOrderDTO;
 import meli.freshfood.model.Product;
-import meli.freshfood.model.PurchaseOrder;
-import meli.freshfood.service.ProductService;
 import meli.freshfood.service.PurchaseOrderService;
-import meli.freshfood.service.PurchaseOrderServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,47 +13,33 @@ import java.util.List;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping("/api/v2/fresh-products")
+@RequestMapping("/api/v1/fresh-products")
 public class PurchaseOrderController {
 
 
     @Autowired
-    private ProductService productService;
-
-    @Autowired
     private PurchaseOrderService purchaseOrderService;
 
-    //veja uma lista completa de produtos
-    @GetMapping
-    public ResponseEntity<List<ProductDTO>> findAll() {
-        return (ResponseEntity<List<ProductDTO>>) ResponseEntity.status(HttpStatus.CREATED);
-    }
 
- /*   //veja uma lista de produtos filtrados por categoria fresco, refrigerado, congelado
-    @GetMapping("/list")
-    public ResponseEntity<List<ProductDTO>> findByCategory(@RequestParam(required = false) String storageType) {
-        List<ProductDTO> list = productService.
-        return null;
-    }
-
-    //registre um pedido de compra
+    //registre um pedido de compra e retorna o valor total do carrinho
     @PostMapping("/orders")
-    public ResponseEntity<List<Product>>(@RequestBody PurchaseOrder purchaseOrder)    {
-        return ResponseEntity.status(HttpStatus.CREATED);
+    public ResponseEntity<Double> create(@RequestBody PurchaseOrderDTO purchaseOrderDTO)    {
+        return ResponseEntity.status(HttpStatus.CREATED).body(purchaseOrderService.create(purchaseOrderDTO));
     }
-*/
 
     //mostrar produtos no pedido
-    @GetMapping("/orders/id")
-    public ResponseEntity<List<PurchaseOrder>> findAllPurchaseOrder() {
-        return (ResponseEntity<List<PurchaseOrder>>) ResponseEntity.status(HttpStatus.CREATED);
+    @GetMapping("/orders")
+    public ResponseEntity<List<Product>> findAllProducts(@RequestBody Long purchaseOrderId) {
+        return ResponseEntity.ok(purchaseOrderService.findAllProducts(purchaseOrderId));
     }
-
+    
     //Modifique o pedido existente para torná-lo do tipo de carrinho ABERTO/FINALIZADO
     @PutMapping("/{id}")
-    public ResponseEntity<PurchaseOrder> statusPurchase(@RequestBody PurchaseOrderDTO purchaseOrderDTO, @RequestParam Long id)  {
-        return new ResponseEntity<PurchaseOrder>(purchaseOrderService.statusPurchase(purchaseOrderDTO, id), HttpStatus.ACCEPTED);
+    public ResponseEntity<String> closePurchaseOrder(@RequestParam Long id)  {
+        purchaseOrderService.closePurchaseOrder(id);
+        return ResponseEntity.ok("Produto atualizado com sucesso!");
     }
+    
 }
 
 
