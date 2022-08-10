db8f4816eb4f9545a1faa03b4b309dd9ca8a19df
everodrigues <evelin.rodrigues@mercadolivre.com>
2022-08-10 16:41:51 -0300

    Adicionando alteraçoes no repositorio de produto

--- src/main/java/meli/freshfood/repository/ProductRepository.java
+++ src/main/java/meli/freshfood/repository/ProductRepository.java
@@ -4,6 +4,10 @@ import meli.freshfood.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findByStorageType(String storageType);
 }
