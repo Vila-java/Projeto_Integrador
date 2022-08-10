285dd86051f4051788b577a7ae3300eb92af12a9
everodrigues <evelin.rodrigues@mercadolivre.com>
2022-08-10 16:47:06 -0300

    Adicionando alterações no repositorio de PurchaseOrder

--- src/main/java/meli/freshfood/repository/PurchaseOrderRepository.java
+++ src/main/java/meli/freshfood/repository/PurchaseOrderRepository.java
@@ -2,7 +2,9 @@ package meli.freshfood.repository;
 
 import meli.freshfood.model.PurchaseOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrder, Long> {
 
 
