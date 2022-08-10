0c7b7ae3442196a810b41281301b4c9bcf4a5705
everodrigues <evelin.rodrigues@mercadolivre.com>
2022-08-10 16:35:35 -0300

    Adicionando modificações no service de PurchaseOrder

--- src/main/java/meli/freshfood/service/PurchaseOrderService.java
+++ src/main/java/meli/freshfood/service/PurchaseOrderService.java
@@ -1,6 +1,8 @@
 package meli.freshfood.service;
 
+import meli.freshfood.dto.ProductDTO;
 import meli.freshfood.dto.PurchaseOrderDTO;
+import meli.freshfood.model.Product;
 import meli.freshfood.model.PurchaseOrder;
 
 import java.util.List;
@@ -8,7 +10,8 @@ import java.util.List;
 
 public interface PurchaseOrderService {
 
-    List<PurchaseOrder> findAllPurchaseOrder();
-    PurchaseOrder statusPurchase(PurchaseOrderDTO purchaseOrderDTO, Long id);
-    Double createPurchaseOrder(PurchaseOrderDTO purchaseOrderDTO);
+    List<Product> findAllProducts(Long purchaseOrderId);
+    void closePurchaseOrder(Long id);
+    void addProductToPurchase(PurchaseOrder purchaseOrder, ProductDTO productDTO);
+    Double create(PurchaseOrderDTO purchaseOrderDTO);
 }
--- src/main/java/meli/freshfood/service/PurchaseOrderServiceImpl.java
+++ src/main/java/meli/freshfood/service/PurchaseOrderServiceImpl.java
@@ -2,15 +2,17 @@ package meli.freshfood.service;
 
 import meli.freshfood.dto.ProductDTO;
 import meli.freshfood.dto.PurchaseOrderDTO;
+import meli.freshfood.model.Client;
 import meli.freshfood.model.Product;
-import meli.freshfood.model.ProductPurchaseOrder;
 import meli.freshfood.model.PurchaseOrder;
+import meli.freshfood.model.StatusPurchaseOrder;
 import meli.freshfood.repository.PurchaseOrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class PurchaseOrderServiceImpl implements PurchaseOrderService{
@@ -21,36 +23,48 @@ public class PurchaseOrderServiceImpl implements PurchaseOrderService{
     @Autowired
     ProductService productService;
 
-    @Override
-    public Double createPurchaseOrder(PurchaseOrderDTO purchaseOrderDTO) {
-        productService.checkBatchAvailableList(purchaseOrderDTO.getProductsDTO());
-        return null;
-    }
+    @Autowired
+    ProductPurchaseOrderService productPurchaseOrderService;
 
-    public PurchaseOrder addProduct(PurchaseOrder purchaseOrder, ProductDTO productDTO) {
-        Product product = productService.findById(productDTO.getProductId()).get();
-   //    ProductPurchaseOrder productPurchaseOrder = new ProductPurchaseOrder();
-        return null;
-    }
+    @Autowired
+    BatchService batchService;
 
-//    @Override
-//    public PurchaseOrder getById(Long id){
-//        return purchaseOrderRepository.findById(id).get();
-//    }
+    @Autowired
+    ClientService clientService;
 
-//   @Override
-//   public List<ProductDTO> productsDto listAll(){
-//        return null;
-//    }
 
     @Override
-    public List<PurchaseOrder> findAllPurchaseOrder() {
-        return purchaseOrderRepository.findAll();
+    public Double create(PurchaseOrderDTO purchaseOrderDTO) {
+        Client client = clientService.findById(purchaseOrderDTO.getBuyerId());
+        PurchaseOrder purchaseOrder = new PurchaseOrder(purchaseOrderDTO, client);
+        purchaseOrderDTO.getProductsDTO().stream().forEach((productDTO) -> {
+            batchService.updateStock(productDTO);
+            addProductToPurchase(purchaseOrder, productDTO);
+        });
+
+        BigDecimal totalPrice = productPurchaseOrderService.totalPriceAllProducts(purchaseOrder);
+        return totalPrice.doubleValue();
     }
 
     @Override
-    public PurchaseOrder statusPurchase(PurchaseOrderDTO purchaseOrderDTO, Long id) {
-        Optional<PurchaseOrder> findShoppingCart = purchaseOrderRepository.findById(id);
-        return null;
+    public void addProductToPurchase(PurchaseOrder purchaseOrder, ProductDTO productDTO) {
+        Product product = productService.findById(productDTO.getProductId());
+        productPurchaseOrderService.create(product, purchaseOrder, productDTO.getQuantity());
+    }
+
+    @Override
+    public List<Product> findAllProducts(Long purchaseOrderId) {
+        PurchaseOrder purchaseOrder = purchaseOrderRepository.findById(purchaseOrderId).get();
+        return purchaseOrder.getProductPurchaseOrders()
+                .stream()
+                .map((productPurchase) -> productPurchase.getProduct())
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void closePurchaseOrder(Long id) {
+        PurchaseOrder purchaseOrder = purchaseOrderRepository.findById(id).get();
+        purchaseOrder.setOrderStatus(StatusPurchaseOrder.DISABLED);
+        purchaseOrderRepository.save(purchaseOrder);
     }
 }
