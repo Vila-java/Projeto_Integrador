67e6818f1b1b5bda31ce6cdee9a5e5c7cae0cef3
everodrigues <evelin.rodrigues@mercadolivre.com>
2022-08-10 16:34:35 -0300

    Adicionando modificações no service de Product

--- src/main/java/meli/freshfood/service/ProductService.java
+++ src/main/java/meli/freshfood/service/ProductService.java
@@ -1,15 +1,12 @@
 package meli.freshfood.service;
 
-import meli.freshfood.dto.ProductDTO;
 import meli.freshfood.model.Product;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ProductService {
-    Optional<Product> findById(Long id);
+
+    Product findById(Long id);
     List<Product> findAll();
-    // List<ProductDTO> findByStorageType();
-    boolean checkBatchAvailable(ProductDTO productDTO);
-    void checkBatchAvailableList(List<ProductDTO> productsDTO);
+    List<Product> findProductByCategory(String storageType);
 }
--- src/main/java/meli/freshfood/service/ProductServiceImpl.java
+++ src/main/java/meli/freshfood/service/ProductServiceImpl.java
@@ -1,16 +1,12 @@
 package meli.freshfood.service;
 
-import meli.freshfood.dto.ProductDTO;
-import meli.freshfood.dto.PurchaseOrderDTO;
 import meli.freshfood.exception.NotFoundException;
-import meli.freshfood.model.Batch;
 import meli.freshfood.model.Product;
 import meli.freshfood.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -18,78 +14,29 @@ public class ProductServiceImpl implements ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-    @Autowired
-    private BatchService batchService;
-
     @Override
-    public Optional<Product> findById(Long id) {
-        Optional<Product> product = productRepository.findById(id);
-
-        if (product.isEmpty()) {
-            throw new NotFoundException("O produto não foi encontrado!");
-        }
-        return product;
+    public Product findById(Long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("O produto não foi encontrado!"));
     }
 
+    // TODO se tiver muito grande a visualização no retorno criar um DTO especifico
     @Override
     public List<Product> findAll() {
-        return productRepository.findAll();
-    }
+        List<Product> listProducts = productRepository.findAll();
 
-    boolean productExists(PurchaseOrderDTO purchaseOrderDTO) {
-        Product product = new Product();
-
-        purchaseOrderDTO.getProductsDTO().stream().map((p) ->
-                findById(product.getProductId())
-        );
-        return true;
-    }
-
-    @Override
-    public boolean checkBatchAvailable(ProductDTO productDTO) {
-        Product product = findById(productDTO.getProductId()).get();
-        Integer totalCapacityAvailable = batchService.totalAvailableBatchesCapacity(product.getBatches());
-
-        if (totalCapacityAvailable < productDTO.getQuantity()) {
-            // TODO: Ajustar exceção
-            throw new NotFoundException("");
+        if(listProducts.isEmpty())  {
+            throw new NotFoundException("A lista de produtos não foi encontrada!");
         }
-        return true;
+        return listProducts;
     }
-
 
     @Override
-    public void checkBatchAvailableList(List<ProductDTO> productsDTO) {
-        productsDTO.stream().forEach((p) -> {
-            checkBatchAvailable(p);
-        });
-    }
-
-
-    public boolean updateStock(PurchaseOrderDTO purchaseOrderDTO) {
-
-
-//        purchaseOrderDTO.getProductsDTO().stream().forEach((pDTO) -> {
-//            checkBatchAvailable(pDTO);
-//            Product product = productRepository.findById(pDTO.getProductId()).get();
-//            Integer productQuantity = pDTO.getQuantity();
-//
-//            List<Batch> batches = product.getBatches();
-//
-//            for(batches : b)
-//
-//
-//            product.getBatches().forEach((b) -> {
-//                productQuantity -= 1;
-//            });
-//
-////            product.getBatches().stream().forEach((b) -> {
-////                b.setCurrentQuantity(b.getCurrentQuantity() - p.getQuantity());
-////                productQuantity -= b.getCurrentQuantity();
-////            });
-//        });
-//
-       return true;
-
+    public List<Product> findProductByCategory(String storageType) {
+       List<Product> listProducts = productRepository.findByStorageType(storageType);
+        if(listProducts.isEmpty())  {
+            throw new NotFoundException("A lista de produtos não foi encontrada!");
+        }
+        return listProducts;
     }
 }
