7ee2cd34729a60ab3bd8af3c26d51c843f5c797f
matheusaalves <matheus.aalves@mercadolivre.com>
2022-08-11 15:10:41 -0300

    Adicionando métodos de pesquisa nos repositórios

--- src/main/java/meli/freshfood/repository/ProductPurchaseOrderRepository.java
+++ src/main/java/meli/freshfood/repository/ProductPurchaseOrderRepository.java
@@ -1,9 +1,15 @@
 package meli.freshfood.repository;
 
+import meli.freshfood.model.Product;
 import meli.freshfood.model.ProductPurchaseOrder;
+import meli.freshfood.model.PurchaseOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductPurchaseOrderRepository extends JpaRepository<ProductPurchaseOrder, Long> {
+    List<ProductPurchaseOrder> findAllByPurchaseOrder(PurchaseOrder purchaseOrder);
+    ProductPurchaseOrder findByPurchaseOrderAndProduct(PurchaseOrder purchaseOrder, Product product);
 }
