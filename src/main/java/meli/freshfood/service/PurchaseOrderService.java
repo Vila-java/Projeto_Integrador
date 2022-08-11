468784a5fba8241e68e1dd53fa9ad2b02ee97e32
matheusaalves <matheus.aalves@mercadolivre.com>
2022-08-11 15:17:29 -0300

    Adicionando tratamento para a inserção e remocão de itens no carrinho

--- src/main/java/meli/freshfood/service/PurchaseOrderService.java
+++ src/main/java/meli/freshfood/service/PurchaseOrderService.java
@@ -1,6 +1,7 @@
 package meli.freshfood.service;
 
 import meli.freshfood.dto.ProductDTO;
+import meli.freshfood.dto.ProductDetailsDTO;
 import meli.freshfood.dto.PurchaseOrderDTO;
 import meli.freshfood.model.Product;
 import meli.freshfood.model.PurchaseOrder;
@@ -10,7 +11,7 @@ import java.util.List;
 
 public interface PurchaseOrderService {
 
-    List<Product> findAllProducts(Long purchaseOrderId);
+    List<ProductDetailsDTO> findAllProducts(Long purchaseOrderId);
     void closePurchaseOrder(Long id);
     void addProductToPurchase(PurchaseOrder purchaseOrder, ProductDTO productDTO);
     Double create(PurchaseOrderDTO purchaseOrderDTO);
