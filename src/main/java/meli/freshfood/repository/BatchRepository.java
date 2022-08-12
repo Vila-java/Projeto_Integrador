ea04d0947b7fe0f9b00b28786d440243a2557bd9
SamanthaPiresLuchmannLeal <samantha.luchmann@mercadolivre.com>
2022-08-12 10:17:37 -0300

    Adicionando método no BatchRepository

--- src/main/java/meli/freshfood/repository/BatchRepository.java
+++ src/main/java/meli/freshfood/repository/BatchRepository.java
@@ -1,9 +1,13 @@
 package meli.freshfood.repository;
 
 import meli.freshfood.model.Batch;
+import meli.freshfood.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BatchRepository extends JpaRepository<Batch, Long> {
+    List<Batch> findAllByProduct(Product product);
 }
