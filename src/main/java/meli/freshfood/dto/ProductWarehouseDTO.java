6c964b8eeee4703d1dfae23e0ab077d095918c65
Bianca Klein Schmitt <bianca.schmitt@mercadolivre.com>
2022-08-12 11:01:28 -0300

    Adicionando modelo DTO para produtos por armazém

--- src/main/java/meli/freshfood/dto/ProductWarehouseDTO.java
+++ src/main/java/meli/freshfood/dto/ProductWarehouseDTO.java
@@ -1,12 +1,13 @@
 package meli.freshfood.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import meli.freshfood.model.Warehouse;
 
 import java.util.List;
 
 @Getter
-
+@AllArgsConstructor
 public class ProductWarehouseDTO {
     private Long productId;
     List<WarehouseDTO> warehousesDTO;
