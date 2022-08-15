b8a2f5647867f2ce1a34eb43bf3712552a1764cf
Weslley Rocha <weslley.rocha@mercadolivre.com>
2022-08-15 09:26:17 -0300

    Adicionando novos atributos no Supervisor.

--- src/main/java/meli/freshfood/model/Supervisor.java
+++ src/main/java/meli/freshfood/model/Supervisor.java
@@ -1,15 +1,21 @@
 package meli.freshfood.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
 import java.util.List;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Supervisor {
 
     @Id
@@ -21,6 +27,19 @@ public class Supervisor {
 
     @Column(nullable = false, length = 60)
     private String lastName;
+
+    @Column(nullable = false, length = 50)
+    @Email(message = "Favor insira um e-mail valido")
+    private String email;
+
+    @Column(nullable = false, length = 11)
+    private String phone;
+
+    @Column(nullable = false,length = 11)
+    private String cpf;
+
+    @Column(nullable = false, length = 20)
+    private String cidade;
 
     @OneToOne(mappedBy = "supervisor")
     @JsonIgnore()
