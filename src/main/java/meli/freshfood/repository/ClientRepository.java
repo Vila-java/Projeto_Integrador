e33a1590e186b49f361bfade53c692e2c76dfc66
Bianca Klein Schmitt <bianca.schmitt@mercadolivre.com>
2022-08-15 18:33:26 -0300

    Adicionando ao cliente a camada repositório

--- src/main/java/meli/freshfood/repository/ClientRepository.java
+++ src/main/java/meli/freshfood/repository/ClientRepository.java
@@ -2,8 +2,13 @@ package meli.freshfood.repository;
 
 import meli.freshfood.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    // @Query(value = "SELECT * FROM WHERE", nativeQuery = true)
 }
