f9e7928d709db2d560181603228c2eaed3b5c527
matheusaalves <matheus.aalves@mercadolivre.com>
2022-08-16 11:13:43 -0300

    [ProductReview] Adicionando método de encontrar todas avaliações por produto e por cliente

--- src/main/java/meli/freshfood/repository/ProductReviewRepository.java
+++ src/main/java/meli/freshfood/repository/ProductReviewRepository.java
@@ -1,9 +1,16 @@
 package meli.freshfood.repository;
 
+import meli.freshfood.dto.ProductReviewClientDTO;
+import meli.freshfood.model.Client;
+import meli.freshfood.model.Product;
 import meli.freshfood.model.ProductReview;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductReviewRepository extends JpaRepository<ProductReview, Long> {
+    List<ProductReview> findAllByProduct(Product product);
+    List<ProductReview> findAllByClient(Client client);
 }
